080c7d1b56cb4187848410eb3baa543b57c316cf
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-01-14 17:25:21 +0500

    added manifest

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import Pomodoro from "../Pomodoro/Pomodoro";
+import React, { lazy, Suspense } from "react";
 import { Wrapper } from "./App.style";
+const Pomodoro = lazy(() => import("../Pomodoro/Pomodoro"));
+
+const renderLoader = () => <p>Loading...</p>;
 
 function App() {
   return (
     <Wrapper data-testid="App">
-      <Pomodoro />
+      <Suspense fallback={renderLoader()}>
+        <Pomodoro />
+      </Suspense>
     </Wrapper>
   );
 }
